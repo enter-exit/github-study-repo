abb15267b23b119581c5d40ad09440e745f7f66b
enter-exit <244606809@qq.com>
2024-12-04 17:00:02 +0800

    异步网络编程练习

--- python/async_client.py
+++ python/async_client.py
@@ -17,6 +17,22 @@
 # if __name__ == '__main__':
 #     asyncio.run(main('127.0.0.1', 5000, 'abc'))
 
+# print('2'.center(100,'-'))
+# import socket
+# import asyncio
+# async def main(ip,port,data):
+#     while True:
+#         c = socket.socket()
+#         loop = asyncio.get_running_loop()
+#         await loop.sock_connect(c,(ip,port))
+#         await loop.sock_sendall(c,data.encode('utf-8'))
+#         res = await loop.sock_recv(c,1024)
+#         await asyncio.sleep(1)
+#         print(res.decode('utf-8'))
+#     c.close()
+# if __name__ == '__main__':
+#     asyncio.run(main('127.0.0.1',5003,'abc'))
+
 print('2'.center(100,'-'))
 import socket
 import asyncio
@@ -27,19 +43,12 @@ async def main(ip,port,data):
         await loop.sock_connect(c,(ip,port))
         await loop.sock_sendall(c,data.encode('utf-8'))
         res = await loop.sock_recv(c,1024)
-        await asyncio.sleep(1)
         print(res.decode('utf-8'))
+        await asyncio.sleep(0.2)
     c.close()
+
 if __name__ == '__main__':
-    asyncio.run(main('127.0.0.1',5003,'abc'))
-
-
-
-
-
-
-
-
+    asyncio.run(main('127.0.0.1',5005,'abc'))
 
 
 
--- python/async_server.py
+++ python/async_server.py
@@ -30,7 +30,35 @@
 # if __name__ == '__main__':
 #     asyncio.run(main('127.0.0.1', 5000))
 
+# print('2'.center(100,'-'))
+# import socket
+# import asyncio
+# async def waiter(loop,conn):
+#     while True:
+#         try:
+#             data = await loop.sock_recv(conn,1024)
+#             if not data:
+#                 break
+#             print(data.decode('utf-8'))
+#             await loop.sock_sendall(conn,data.upper())
+#         except:
+#             break
+#     conn.close()
+# async def main(ip,port):
+#     s = socket.socket()
+#     s.bind((ip,port))
+#     s.listen(2)
+#     s.setblocking(False)
+#     loop = asyncio.get_running_loop()
+#     while True:
+#         conn,addr = await loop.sock_accept(s)
+#         asyncio.create_task(waiter(loop,conn))
+#
+# if __name__ == '__main__':
+#     asyncio.run(main('127.0.0.1',5003))
+
 print('2'.center(100,'-'))
+
 import socket
 import asyncio
 async def waiter(loop,conn):
@@ -44,18 +72,20 @@ async def waiter(loop,conn):
         except:
             break
     conn.close()
+
 async def main(ip,port):
     s = socket.socket()
     s.bind((ip,port))
-    s.listen(2)
+    s.listen(3)
     s.setblocking(False)
     loop = asyncio.get_running_loop()
     while True:
         conn,addr = await loop.sock_accept(s)
-        asyncio.create_task(waiter(loop,conn))
+        await loop.create_task(waiter(loop,conn))
 
 if __name__ == '__main__':
-    asyncio.run(main('127.0.0.1',5003))
+    asyncio.run(main('127.0.0.1',5005))
+
 
 
 
