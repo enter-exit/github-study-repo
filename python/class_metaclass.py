d449f9e57ecf021f6b83fbbd7bb8eeb888dd10d9
enter-exit <244606809@qq.com>
2024-12-09 09:44:49 +0800

    class练习4

--- python/class_metaclass.py
+++ python/class_metaclass.py
@@ -58,13 +58,41 @@ from struct import pack_into
 # print(obj,obj2)
 # print(obj is obj2)
 
-print('3'.center(100,'-'))
+# print('3'.center(100,'-'))
+# class A(type):
+#     obj = None
+#     def __new__(cls, *args, **kwargs):
+#         print('A-new')
+#         return super().__new__(cls,*args,**kwargs)
+#
+#     def __init__(self,*args,**kwargs):
+#         print('A-init')
+#     def __call__(self, *args, **kwargs):
+#         if not self.obj:
+#             self.obj = self.__new__(self)
+#         self.__init__(self.obj)
+#         return self.obj
+# # B = A('B',(),{})
+# # B = A.__call__(A,'B',(),{})
+# class B(metaclass=A):
+#     def __new__(cls, *args, **kwargs):
+#         print('B-new')
+#         return super().__new__(cls,*args,**kwargs)
+#     def __init__(self):
+#         print('B-init')
+#
+# obj = B()
+# obj2 = B.__call__()
+# print(obj,obj2)
+# print(obj is obj2)
+
+print('4'.center(100,'-'))
+
 class A(type):
     obj = None
     def __new__(cls, *args, **kwargs):
         print('A-new')
         return super().__new__(cls,*args,**kwargs)
-
     def __init__(self,*args,**kwargs):
         print('A-init')
     def __call__(self, *args, **kwargs):
@@ -72,6 +100,7 @@ class A(type):
             self.obj = self.__new__(self)
         self.__init__(self.obj)
         return self.obj
+
 # B = A('B',(),{})
 # B = A.__call__(A,'B',(),{})
 class B(metaclass=A):
@@ -85,6 +114,7 @@ obj = B()
 obj2 = B.__call__()
 print(obj,obj2)
 print(obj is obj2)
+
 
 
 
