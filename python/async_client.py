dcb02b19ef02f1ee99572b7292e5ef0607191311
enter-exit <244606809@qq.com>
2024-12-10 09:24:47 +0800

    异步网络编程练习4

--- python/async_client.py
+++ python/async_client.py
@@ -33,22 +33,61 @@
 # if __name__ == '__main__':
 #     asyncio.run(main('127.0.0.1',5003,'abc'))
 
-print('2'.center(100,'-'))
+# print('3'.center(100,'-'))
+# import socket
+# import asyncio
+# async def main(ip,port,data):
+#     while True:
+#         c = socket.socket()
+#         loop = asyncio.get_running_loop()
+#         await loop.sock_connect(c,(ip,port))
+#         await loop.sock_sendall(c,data.encode('utf-8'))
+#         res = await loop.sock_recv(c,1024)
+#         print(res.decode('utf-8'))
+#         await asyncio.sleep(0.2)
+#     c.close()
+#
+# if __name__ == '__main__':
+#     asyncio.run(main('127.0.0.1',5007,'abc'))
+
+print('4'.center(100,'-'))
 import socket
+import datetime
 import asyncio
 async def main(ip,port,data):
     while True:
-        c = socket.socket()
         loop = asyncio.get_running_loop()
+        c = socket.socket()
         await loop.sock_connect(c,(ip,port))
         await loop.sock_sendall(c,data.encode('utf-8'))
         res = await loop.sock_recv(c,1024)
         print(res.decode('utf-8'))
-        await asyncio.sleep(0.2)
+        print(datetime.datetime.now())
     c.close()
 
+
 if __name__ == '__main__':
-    asyncio.run(main('127.0.0.1',5005,'abc'))
+    asyncio.run(main('127.0.0.1',6000,'abcdefg'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- python/async_server.py
+++ python/async_server.py
@@ -29,6 +29,7 @@
 #
 # if __name__ == '__main__':
 #     asyncio.run(main('127.0.0.1', 5000))
+from tkinter import BaseWidget
 
 # print('2'.center(100,'-'))
 # import socket
@@ -57,9 +58,38 @@
 # if __name__ == '__main__':
 #     asyncio.run(main('127.0.0.1',5003))
 
-print('2'.center(100,'-'))
+# print('3'.center(100,'-'))
+#
+# import socket
+# import asyncio
+# async def waiter(loop,conn):
+#     while True:
+#         try:
+#             data = await loop.sock_recv(conn,1024)
+#             if not data:
+#                 break
+#             print(data.decode('utf-8'))
+#             await loop.sock_sendall(conn,data.upper())
+#         except:
+#             break
+#     conn.close()
+#
+# async def main(ip,port):
+#     s = socket.socket()
+#     s.bind((ip,port))
+#     s.listen(3)
+#     s.setblocking(False)
+#     loop = asyncio.get_running_loop()
+#     while True:
+#         conn,addr = await loop.sock_accept(s)
+#         await loop.create_task(waiter(loop,conn))
+#
+# if __name__ == '__main__':
+#     asyncio.run(main('127.0.0.1',5007))
 
+print('4'.center(100,'-'))
 import socket
+import datetime
 import asyncio
 async def waiter(loop,conn):
     while True:
@@ -68,25 +98,24 @@ async def waiter(loop,conn):
             if not data:
                 break
             print(data.decode('utf-8'))
+            await asyncio.sleep(0.5)
+            print(datetime.datetime.now())
             await loop.sock_sendall(conn,data.upper())
         except:
             break
     conn.close()
-
 async def main(ip,port):
+    loop = asyncio.get_running_loop()
     s = socket.socket()
     s.bind((ip,port))
     s.listen(3)
     s.setblocking(False)
-    loop = asyncio.get_running_loop()
     while True:
         conn,addr = await loop.sock_accept(s)
-        await loop.create_task(waiter(loop,conn))
+        asyncio.create_task(waiter(loop,conn))
 
 if __name__ == '__main__':
-    asyncio.run(main('127.0.0.1',5005))
-
-
+    asyncio.run(main('127.0.0.1',6000))
 
 
 
